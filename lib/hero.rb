19d694972f68e033f36a3235f06a2494cd3e0b04
Luciano <lucianodantas53@gmail.com>
2023-01-11 19:52:12 -0300

    Committing hero.rb file

---
+++ lib/hero.rb
@@ -0,0 +1,7 @@
+class Hero
+  attr_accessor :weapon #Criando o atributo 'weapon'(arma).
+
+  def initialize #É o construtor da classe.
+    @weapon = 'sword' #Define que a arma será uma espeda "sword".
+  end
+end
