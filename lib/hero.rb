014dea33152e9a1378971a940246cd8836c3e57d
gudiao21 <lucianodantas53@gmail.com>
2023-03-09 19:15:50 -0300

    Committing .rspec, lib/hero.rb e spec/lib/hero_spec.rb files

--- lib/hero.rb
+++ lib/hero.rb
@@ -2,6 +2,11 @@ class Hero
   attr_accessor :weapon #Criando o atributo 'weapon'(arma).
 
   def initialize #É o construtor da classe.
-    @weapon = 'sword' #Define que a arma será uma espeda "sword".
+    @weapon = 'sword' #Define que a arma será uma espeda "sword".    
   end
+
+  def hp
+    1001
+  end
+
 end
--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -1,9 +1,16 @@
-require_relative 'spec_helper' #Se refere ao aquivo 'spec_helper.rb'.
-require_relative 'hero' #Se refere ao arquivo 'hero.rb'.
+#Não é para fazer agora, é para deixar mais para frente, segundo orientação do Professor.
+require '/home/luciano/cursorspec/spec/spec_helper' #Se refere ao aquivo 'spec_helper.rb'.
+require '/home/luciano/cursorspec/lib/hero' #Se refere ao arquivo 'hero.rb'.
 
 describe Hero do
   it 'has a sword' do
     hero = Hero.new #Criação do objeto 'hero' dentro da classe 'Hero'.
     expect(hero.weapon).to eq('sword') #"expect"verifica se o comportamento esperado está acontecendo, se dentro do herói tem um atributo weapon(arma), do tipo espada('sword').
+
+  end
+
+  it 'has more than 1000 HP points' do
+    hero = Hero.new
+    expect(hero.hp).to be > 1000
   end
 end
